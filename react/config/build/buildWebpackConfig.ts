3434e078c9774ff86755096dff28b76f102005d8
Irina Tumanova <tymanira@mail.ru>
2024-07-13 12:37:10 +0500

    добавлена оптимизация вебпака

--- react/config/build/buildWebpackConfig.ts
+++ react/config/build/buildWebpackConfig.ts
@@ -1,22 +1,29 @@
-import { Configuration } from 'webpack';
+import {Configuration} from 'webpack';
 
-import { buildDevServer } from './buildDevServer';
-import { buildLoaders } from './buildLoaders';
-import { buildPlugins } from './buildPlugins';
-import { buildResolvers } from './buildResolvers';
-import { BuildOptions } from './types/config';
+import {buildDevServer} from './buildDevServer';
+import {buildLoaders} from './buildLoaders';
+import {buildPlugins} from './buildPlugins';
+import {buildResolvers} from './buildResolvers';
+import {BuildOptions} from './types/config';
 
 export function buildWebpackConfig(options: BuildOptions): Configuration {
-    const { paths, mode, isDev } = options;
+    const {paths, mode, isDev} = options;
 
     return {
         mode,
+        target: 'web',
         entry: paths.entry,
         output: {
             filename: 'js/[name].[contenthash].js',
             path: paths.build,
             clean: true,
             publicPath: '/'
+        },
+        optimization: {
+            minimize: true,
+            moduleIds: 'deterministic',
+            innerGraph: true,
+            concatenateModules: true,
         },
         module: {
             rules: buildLoaders(options),
